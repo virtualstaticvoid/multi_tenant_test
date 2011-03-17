a7d7eb565cb6b8ae79934d035e8a8d2cdeb8fbdd
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-03-17 13:01:38 +0200

    rails new multitenant1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multitenant1::Application.config.secret_token = 'e6afda851d42d1b9f2a036d009d4a1c71a69ce00a479e9e3bcf7448894bdebc7d83e6607b7ea02898107655e1727367bc5b73374a65d02a14f2a9c7a9679e27d'
