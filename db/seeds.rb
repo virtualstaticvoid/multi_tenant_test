61f69911c6cc3ba8589cc030c1f097bfad2e6aff
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2011-03-17 13:41:13 +0200

    Manually added seed data for tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+
+# clean up
+Property.destroy_all
+Account.destroy_all
+
+# create stock
+Account.create!(:subdomain => 'test1')
+Account.create!(:subdomain => 'test2')
